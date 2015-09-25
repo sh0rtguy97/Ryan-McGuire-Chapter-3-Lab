b2408be4ac27b06e58ed9c056111269f46c5b1d7
unknown <4068886@HS222-14.vtsd.com>
2015-09-25 11:54:00 -0400

    Chapter3Lab

---
+++ Lab03Dst.java
@@ -0,0 +1,51 @@
+// Lab03Dst.java
+// The Visible Average Program
+// This is the student, starting version, of Lab 03C.
+
+
+public class Lab03Dst
+{
+	public static void main(String args[])
+	{
+		System.out.println("Lab 03D");
+		System.out.println();
+		int num1 = 11;
+		int num2 = 22;
+		int num3 = 33;
+		int num4 = 44;
+		int num5 = 55;
+		int sum=(num1+num2+num3+num4+num5);
+		int average=(num1+num2+num3+num4+num5)/5;
+		
+		System.out.println();
+		System.out.println("Number 1="+num1);
+		System.out.println("Number 2="+num2);
+		System.out.println("Number 3="+num3);
+		System.out.println("Number 4="+num4);
+		System.out.println("Number 5="+num5);
+		System.out.println("Total"+"="+sum);
+		System.out.println("Average=" +average);
+		System.out.println();
+		
+		System.out.println("100-Point Version");
+		double number1 = 15.2;
+		double number2 = 17.9;
+		double number3 = 20.1;
+		double number4 = 22;
+		double number5 = 30;
+		double realsum=(number1+number2+number3+number4+number5);
+		double realaverage=(number1+number2+number3+number4+number5)/5;
+		
+		System.out.println();
+		System.out.println("Real Number 1="+number1);
+		System.out.println("Real Number 2="+number2);
+		System.out.println("Real Number 3="+number3);
+		System.out.println("Real Number 4="+number4);
+		System.out.println("Real Number 5="+number5);
+		System.out.println("Total="+realsum);
+		System.out.println("Average="+realaverage);
+		
+
+		System.out.println();
+	}
+}
